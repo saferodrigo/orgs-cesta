194ee45f93dae6122ea56cd401ce2bc9081ff5c2
Rodrigo <rodrigo.ferreira.sa25@gmail.com>
2023-04-14 16:59:29 -0300

    commit initial

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat';
+
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontCarregada) {
+    return <View />
+  }
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
+  )
+}
